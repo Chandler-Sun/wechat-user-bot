e8779a711375c1333b6d5056d248e6c514f6002a
Chandler-Sun <chandler@misfit.com>
2016-12-08 18:03:43 +0800

    cache wxSession

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var login = require('./lib/webwx.js').login;
 var getbaseRequest = require('./lib/webwx.js').getbaseRequest;
 var webwxinit = require('./lib/webwx.js').webwxinit;
 var messageQueue = require('./lib/reply/messageQueue.js').messageQueue;
-
 var wechatLogger = require('./lib/logger/logger.js').wechatLogger;
 var generateReply = require('./lib/reply/reply.js').generateReply;
 
@@ -19,18 +18,21 @@ var robot = require('./lib/robot.js').robot;
 // display, which is a stream
 var child_process = require('child_process');
 var display = child_process.spawn('display');
+var fs = require('fs');
 
-getUUID
-  .then(checkAndParseUUID)
-  .then(showQRImage(display))
-  .then(checkLogin)
-  .then(parseRedirectUrl)
-  .then(login)
-  .then(getbaseRequest)
-  .then(webwxinit)
-  .then(webwxgetcontact)
-  .then(messageQueue)
-  .then(robot(
+var cachedWXSession = require('./config/wxSession.cache.json')
+
+var serializeWXSession = function(wxSession) {
+  return new Promise((resolve, reject)=>{
+    fs.writeFile('./config/wxSession.cache.json', JSON.stringify(wxSession), (err)=>{
+      if(err) return reject(err)
+      resolve(wxSession)
+    })
+  })
+}
+
+if(cachedWXSession.BaseRequest){
+  messageQueue(cachedWXSession).then(robot(
     [(wxSession)=>o=>true],
     [wechatLogger, generateReply]
     // [],
@@ -38,6 +40,33 @@ getUUID
   ))
   .catch((e)=>{
     console.error(e);
+    fs.writeFileSync('./config/wxSession.cache.json', JSON.stringify({}))
     process.exit(1);
   });
+}else{
+  getUUID
+    .then(checkAndParseUUID)
+    .then(showQRImage(display))
+    .then(checkLogin)
+    .then(parseRedirectUrl)
+    .then(login)
+    .then(getbaseRequest)
+    .then(webwxinit)
+    .then(webwxgetcontact)
+    .then(serializeWXSession)
+    .then(messageQueue)
+    .then(robot(
+      [(wxSession)=>o=>true],
+      [wechatLogger, generateReply]
+      // [],
+      // [wechatLogger]
+    ))
+    .catch((e)=>{
+      console.error(e);
+      fs.writeFileSync('./config/wxSession.cache.json', JSON.stringify({}))
+      process.exit(1);
+    });
+}
+
+
 
